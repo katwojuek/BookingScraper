4020c30c83bec35f563adc4d89ff6d436ff484aa
katwojuek <wojcikk@uek.krakow.pl>
2022-03-22 12:58:04 +0100

    2022-03-22 KW

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # BookingScraper
+
+## Structure of single opinion
+
+|Element|Selector|Variable|
+|-------|--------|--------|
+|review|li.review_list_new_item_block|
+|review id||
+|subject||
+|pros||
+|cons||
+|score||
+|review date||
+|review language||
+|author||
+|country||
+|room type||
+|gues type||
+|staying||
--- scraper.py
+++ scraper.py
@@ -1,7 +1,11 @@
-from urllib import response
 import requests
+from bs4 import BeautifulSoup
 
-url = "https://www.booking.com/hotel/pl/happy-tower.html"
+url = "https://www.booking.com/hotel/pl/happy-tower.html#tab-reviews"
 response = requests.get(url)
-print(response.status_code)
-print(response.text)
+
+page_dom = BeautifulSoup(response.text, 'html.parser')
+
+reviews = page_dom.select("script[data-capla-store-data='apollo']")
+review = reviews.pop(-1)
+print(review)
